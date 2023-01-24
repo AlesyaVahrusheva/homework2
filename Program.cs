190b96b400736a22b6ec6f2d87d9ac15c682d319
AlesyaVahrusheva <vahrushevaao@yandex.ru>
2023-01-24 15:26:54 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Принимает 3 числа и выдает наибольшее
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+Console.Write("Наибольшее число: max = ");
+
+if (b > max)
+{
+  max=b;  
+}
+if (c > max)
+{
+   max=c;  
+}
+
+Console.Write(max);
+
